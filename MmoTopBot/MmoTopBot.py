16414e121aea465f935fb1a2ca4c1a8be4d605e8
md74671 <mcditchman@gmail.com>
2015-12-18 10:50:23 -0500

    Update MmoTopBot.py
    Updated variable names and comments

--- MmoTopBot/MmoTopBot.py
+++ MmoTopBot/MmoTopBot.py
@@ -2,20 +2,19 @@
 import string
 import OAuth2Util
 
-
 subreddits = list()
-r = praw.Reddit('Top MMORPG post aggregator by /u/SadDragon')
+reddit = praw.Reddit('Top MMORPG post aggregator')
 
 # Setup Authentication
-o = OAuth2Util.OAuth2Util(r)
-o.refresh(force=True)
-
+oAuth = OAuth2Util.OAuth2Util(reddit)
+oAuth.refresh(force=True)
 
 print('!! MMORPG Top Bot has started !!')
-# Get MMORPG sidebar info
-sidebar =  r.get_subreddit('mmorpg').description.split('\n')
 
-# Grab popular MMO list
+# Get MMORPG sidebar info
+sidebar =  reddit.get_subreddit('mmorpg').description.split('\n')
+
+# Grab popular MMO list from sidebar
 index = 0
 start = False
 for line in sidebar:
@@ -30,18 +29,20 @@ for line in sidebar:
         start = False
         break
 
+# Create the output
 print('Creating output...');
 output = 'Greetings adventurers!\n\nI have compiled a list of all the top posts from last week for all of the popular MMO subreddits in the sidebar. It may be news or it may be fluff but its all here in one place!\n\n'
 output += 'Subreddit | Top Post Last Week | Points\n---|---|---\n'
 
-# Loop through subreddits and get top from week
+# Loop through subreddits and get top post from past week 
 for sub in subreddits:
-    submissions = r.get_subreddit(sub).get_top_from_week(limit=1)
+    submissions = reddit.get_subreddit(sub).get_top_from_week(limit=1)
     for post in submissions:
         output += '/r/{0} | [{1}]({2}) | {3}\n'.format(sub, post.title, post.url, post.score)
     
 output += '\n\n---\n\n I am a bot and this is an automated post. Please direct any questions to the mods via modmail.\n\n*beep boop*'
 
+# Submit post to subreddit
 print('Making post...');
-r.submit('mmorpg','Top MMO posts last week, tonight!',output)
+reddit.submit('mmorpg','Top MMO posts last week, tonight!',output)
 
